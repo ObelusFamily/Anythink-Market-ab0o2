c54208a59ba94e86540e5eb2285411930a8f703f
Curtis Updike <cjupdike@icloud.com>
2022-08-25 15:32:45 -0400

    filter api items by title

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -103,6 +103,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
     async def filter_items(  # noqa: WPS211
         self,
         *,
+        title: Optional[str] = None,
         tag: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
@@ -136,6 +137,9 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
             ),
         )
         # fmt: on
+
+        if title:
+            query = query.where(items.title == title)
 
         if tag:
             query_params.append(tag)
