7f023c535c1b9351ba98cd8318090fdc18eb956e
Curtis Updike <cjupdike@icloud.com>
2022-08-30 11:38:40 -0400

    Hide input until secret click

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import logo from "../../imgs/logo.png";
 
-const Banner = ({ inputValue, onInputChange }) => (
-  <div className="banner text-white">
-    <div className="container p-4 text-center">
-      <img src={logo} alt="banner" />
-      <div className="form-inline">
-        <span id="get-part">A place to get</span>
-        <input
-          className="form-control flex-grow-1 m-4"
-          id="search-box"
-          type="search"
-          placeholder="What is it that you truly desire?"
-          value={inputValue}
-          onChange={onInputChange}
-        />
-        <span> the cool stuff.</span>
+const Banner = ({ inputValue, onInputChange }) => {
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div className="form-inline">
+          <span id="get-part">
+            A place to <span onClick={() => setHidden(false)}>get</span>
+          </span>
+          {hidden ? (
+            <pre> </pre>
+          ) : (
+            <input
+              className="form-control flex-grow-1 m-4"
+              id="search-box"
+              type="search"
+              placeholder="What is it that you truly desire?"
+              value={inputValue}
+              onChange={onInputChange}
+            />
+          )}
+          <span>the cool stuff.</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Banner;
